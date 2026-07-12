80c9326626920296ae98b2f3a5083bd635093130
LycheeLihing <masakirules@gmail.com>
2026-07-12 23:09:14 +0800

    refactored pipeline to python

--- utils/logger.js
+++
@@ -1,6 +0,0 @@
-function log(message, level = 'info') {
-  const timestamp = new Date().toISOString();
-  console.log(`[${timestamp}] [${level.toUpperCase()}] ${message}`);
-}
-
-module.exports = {log};
